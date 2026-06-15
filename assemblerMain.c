098c18fbc31820c0b2527a58f82e64661853364c
Guy Even <guyeven2610@gmail.com>
2026-06-15 18:22:14 +0300

    Remove invalid encoded comments from constants

--- assemblerMain.c
+++ assemblerMain.c
@@ -4,9 +4,9 @@
 #include "FirstPass.h"
 #include "label.h"
 #include "ScondPass.h"
-#define MAX_LABEL_LEN 31/*אורך התווית*/
-#define MAX_ARR_LEN 150/*אורך המערכים*/
-#define WRD_ARR_BITS_LEN 12/*אורך המחרוזת של הביטים*/
+#define MAX_LABEL_LEN 31
+#define MAX_ARR_LEN 150
+#define WRD_ARR_BITS_LEN 12
 #define TABLE_SIZE (sizeof(lookuptable)/sizeof(code))
 #define MAX_LINE_LENGTH 80
 #define IcStart 100
